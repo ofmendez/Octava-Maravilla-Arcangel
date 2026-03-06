2c5219457794a39c0cdd2aeb355e9ff635ff1df3
Andrés <andresgarcia@tretto.co>
2026-03-06 14:45:00 -0500

    New dates

--- dates.js
+++ dates.js
@@ -1,7 +1,7 @@
 // dates.js
 // Formato: dateISO (YYYY-MM-DD), city, country, venue, ticketUrl, saleStartUTC (opcional)
 window.TOUR_DATES = [
-  { dateISO: "2026-05-13", city: "Barcelona", country: "España", venue: "Sant Jordi Club", ticketUrl: "https://www.livenation.es/show/1656664" },
+  { dateISO: "2026-05-13", city: "Barcelona", country: "España", venue: "Sant Jordi Club", ticketUrl: "", soldOut: true },
   { dateISO: "2026-05-14", city: "Madrid", country: "España", venue: "Movistar Arena", ticketUrl: "", soldOut: true },
   { dateISO: "2026-07-30", city: "Quito", country: "Ecuador", venue: "Coliseo Rumiñahui", ticketUrl: "", soldOut: true },
   { dateISO: "2026-08-01", city: "Guayaquil", country: "Ecuador", venue: "Coliseo Voltaire", ticketUrl: "", soldOut: true },
@@ -13,16 +13,17 @@ window.TOUR_DATES = [
   { dateISO: "2026-08-23", city: "Guadalajara", country: "Mexico", venue: "Auditorio Telmex", ticketUrl: "http://www.ticketmaster.com.mx/arcangel-zapopan-jalisco-08-23-2026/event/1400644CADB69F25" },
   { dateISO: "2026-08-28", city: "Buenos Aires", country: "Argentina", venue: "Movistar Arena", ticketUrl: "", soldOut: true },
   { dateISO: "2026-08-31", city: "Santiago", country: "Chile", venue: "Movistar Arena", ticketUrl: "", soldOut: true },
+  { dateISO: "2026-09-01", city: "Santiago", country: "Chile", venue: "Movistar Arena", ticketUrl: "", soldOut: true },
   { dateISO: "2026-09-05", city: "Medellín", country: "Colombia", venue: "Estadio Atanasio Girardot", ticketUrl: "", soldOut: true },
-  { dateISO: "2026-09-11", city: "La Paz", country: "Bolivia", venue: "Teatro al Aire Libre", ticketUrl: "https://todotix.com.bo/ticket/arcangel", isNew: true },
+  { dateISO: "2026-09-11", city: "La Paz", country: "Bolivia", venue: "Teatro al Aire Libre", ticketUrl: "https://todotix.com.bo/ticket/arcangel" },
   { dateISO: "2026-09-12", city: "Lima", country: "Perú", venue: "Estadio San Marcos", ticketUrl: "https://www.ticketmaster.pe/event/arcangel-la-8va-maravilla-world-tour-home" },
   { dateISO: "2026-09-17", city: "Tegucigalpa", country: "Honduras", venue: "Estadio Chochi Sosa", ticketUrl: "" },
   { dateISO: "2026-09-19", city: "Cdad. de Guatemala", country: "Guatemala", venue: "Explanada 5", ticketUrl: "" },
   { dateISO: "2026-09-24", city: "Cdad. de Panamá", country: "Panamá", venue: "CC Amador", ticketUrl: "" },
   { dateISO: "2026-09-26", city: "Alajuela", country: "Costa Rica", venue: "Parque Viva", ticketUrl: "" },
   { dateISO: "2026-08-14", city: "CDMX", country: "México", venue: "Palacio de los Deportes", ticketUrl: "https://www.ticketmaster.com.mx/arcangel-ciudad-de-mexico-14-08-2026/event/14006446A1F56F31", isNew: true },
-  { dateISO: "2026-08-30", city: "Buenos Aires", country: "Argentina", venue: "Movistar Arena", ticketUrl: "https://www.movistararena.com.ar/show/c51ec5d6-233b-4133-8d16-b169c34da639", isNew: true},
-  { dateISO: "2026-08-03", city: "Guayaquil", country: "Ecuador", venue: "Coliseo Voltaire", ticketUrl: "https://www.ticketshow.com.ec/evento/Arcangel-en-Guayaquil-2026", isNew: true },
-  { dateISO: "2026-07-29", city: "Quito", country: "Ecuador", venue: "Coliseo Rumiñahui", ticketUrl: "https://www.ticketshow.com.ec/evento/ARCANGEL-QUITO-2026", isNew: true },
+  { dateISO: "2026-08-30", city: "Buenos Aires", country: "Argentina", venue: "Movistar Arena", ticketUrl: "", soldOut: true},
+  { dateISO: "2026-08-03", city: "Guayaquil", country: "Ecuador", venue: "Coliseo Voltaire", ticketUrl: "", soldOut: true },
+  { dateISO: "2026-07-29", city: "Quito", country: "Ecuador", venue: "Coliseo Rumiñahui", ticketUrl: "", soldOut: true},
   { dateISO: "2026-05-11", city: "Madrid", country: "España", venue: "Movistar Arena", ticketUrl: "https://www.livenation.es/show/1658095", isNew: true }
 ];
