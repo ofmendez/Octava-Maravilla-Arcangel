2539b4461db98900eaf1af0c234ca6ccb8c87d60
Andrés <andresgarcia@tretto.co>
2026-08-12 13:28:48 -0500

    Fechas12 de agosto

--- dates.js
+++ dates.js
@@ -2,9 +2,7 @@
 // Formato: dateISO (YYYY-MM-DD), city, country, venue, ticketUrl, saleStartUTC (opcional)
 window.TOUR_DATES = [
   { dateISO: "2026-08-13", city: "CDMX", country: "México", venue: "Palacio de los Deportes", ticketUrl: "", soldOut: true },
-  { dateISO: "2026-08-18", city: "Puebla", country: "Mexico", venue: "Auditorio GNP", ticketUrl: "https://www.eticket.mx/masinformacion.aspx?idevento=35191" },
   { dateISO: "2026-08-20", city: "Mérida", country: "Mexico", venue: "Foro GNP", ticketUrl: "https://www.eticket.mx/masinformacion.aspx?idevento=35208" },
-  { dateISO: "2026-08-22", city: "Querétaro", country: "Mexico", venue: "Auditorio Josefa", ticketUrl: "https://www.eticket.mx/masinformacion.aspx?idevento=35192" },
   { dateISO: "2026-08-23", city: "Guadalajara", country: "Mexico", venue: "Auditorio Telmex", ticketUrl: "http://www.ticketmaster.com.mx/arcangel-zapopan-jalisco-08-23-2026/event/1400644CADB69F25" },
   { dateISO: "2026-08-28", city: "Buenos Aires", country: "Argentina", venue: "Movistar Arena", ticketUrl: "", soldOut: true },
   { dateISO: "2026-08-31", city: "Santiago", country: "Chile", venue: "Movistar Arena", ticketUrl: "", soldOut: true },
